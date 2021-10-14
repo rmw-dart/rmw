e7625f391608274ac6253affb4d47e9dd8cfaf1e
gcxfd <i@rmw.link>
2021-10-14 13:49:25 +0800

    🔶

--- bin/rmw.dart
+++ bin/rmw.dart
@@ -6,30 +6,25 @@ import 'package:rmw/boot.dart';
 import 'package:rmw/version.dart' show packageVersion;
 import 'package:args/args.dart' show ArgParser;
 
-
 void main(List<String> arguments) async {
-
   final args = ArgParser();
-  args.addOption("dir", abbr: 'd', help:'工作目录');
-  args.addOption("help", abbr: 'h', help:'帮助');
+  args.addOption("dir", abbr: 'd', help: '工作目录');
+  args.addOption("help", abbr: 'h', help: '帮助');
 
   print("人民网络 $packageVersion\n打倒数据霸权 · 网络土地革命！\nhttps://rmw.link\n");
 
   final config = args.parse(arguments);
-  if (config['help']!=null) {
+  if (config['help'] != null) {
     print(args.usage);
     return;
   }
 
-  final root = config['dir']??
+  final root = config['dir'] ??
       join(
           Platform.environment['HOME'] ??
-          Platform.environment['USERPROFILE'] ??
-          dirname(Platform.script.toFilePath()),
-          '.rmw'
-      );
+              Platform.environment['USERPROFILE'] ??
+              dirname(Platform.script.toFilePath()),
+          '.rmw');
 
-  await boot(
-      root
-  );
+  await boot(root);
 }
--- lib/boot.dart
+++ lib/boot.dart
@@ -3,16 +3,47 @@ import 'db.dart';
 import "package:path/path.dart" show join;
 import 'dart:io' show Directory;
 
+import 'dart:core';
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+
+Future<InternetAddress> ip() async {
+  InternetAddress result;
+
+  int code = Random().nextInt(255);
+  var dgSocket = await RawDatagramSocket.bind(InternetAddress.anyIPv4, 0);
+  dgSocket.readEventsEnabled = true;
+  dgSocket.broadcastEnabled = true;
+  final ret = dgSocket.timeout(Duration(milliseconds: 200), onTimeout: (sink) {
+    sink.close();
+  }).expand<InternetAddress>((event) {
+    if (event == RawSocketEvent.read) {
+      final dg = dgSocket.receive();
+      if (dg != null && dg.data.length == 1 && dg.data[0] == code) {
+        dgSocket.close();
+        return [dg.address];
+      }
+    }
+    return [];
+  }).first;
+
+  dgSocket.send([code], InternetAddress("255.255.255.255"), dgSocket.port);
+  return ret;
+}
+
 Future<void> boot(String root) async {
+  print(await ip());
+
   await Directory(root).create(recursive: true);
 
-  final store = openStore(directory:join(root,"box"));
+  final store = openStore(directory: join(root, "box"));
 
   final box = store.box<User>();
-  final user = User(name:'good');
+  final user = User(name: 'good');
   print(box.put(user));
 
-  for(final i in box.getAll()){
+  for (final i in box.getAll()) {
     print("${i.id} ${i.name}");
   }
   store.close();
