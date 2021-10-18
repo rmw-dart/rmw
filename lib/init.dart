4aade67076815dcc721c5490d04249fd3bffe044
gcxfd <i@rmw.link>
2021-10-18 16:32:33 +0800

    🔶

--- lib/init.dart
+++ lib/init.dart
@@ -2,11 +2,30 @@ import 'objectbox.g.dart';
 import 'db/user.dart';
 
 import "package:path/path.dart" show join;
-import 'dart:io' show Directory;
-import 'package:intranet_ip/intranet_ip.dart';
+import 'dart:io'
+    show Directory, RawDatagramSocket, InternetAddress, SocketException;
+import 'package:upnp_port_forward/init.dart' show UpnpPortForwardDaemon;
 
 Future<void> init(String root) async {
-  print((await intranetIpv4()).rawAddress);
+  late final RawDatagramSocket udp;
+
+  try {
+    udp = await RawDatagramSocket.bind(InternetAddress.anyIPv4, 0);
+  } on SocketException catch (e) {
+    print(e.osError);
+    print(e.osError?.errorCode);
+    return;
+  }
+
+  final port = udp.port;
+
+  UpnpPortForwardDaemon('rmw.link', (protocol, port, state) {
+    print("upnp port mapped : $protocol $port $state");
+  })
+    ..udp(port)
+    ..run();
+
+  print(udp.port);
 
   await Directory(root).create(recursive: true);
 
